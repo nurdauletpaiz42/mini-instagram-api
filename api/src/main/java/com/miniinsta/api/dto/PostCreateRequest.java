f94200ad9f35f0f352bc130f1c6090cc8bc2c5dc
nurdauletpaiz42 <nurdauletpaiz42@gmail.com>
2025-11-24 20:41:05 +0500

    Delete api directory

--- api/src/main/java/com/miniinsta/api/dto/PostCreateRequest.java
+++
@@ -1,9 +0,0 @@
-package com.miniinsta.api.dto;
-
-import lombok.Data;
-
-@Data
-public class PostCreateRequest {
-    private Long authorId; // Кто пишет пост (пока передаем вручную)
-    private String caption; // Текст поста
-}
--- api/src/main/java/com/miniinsta/api/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.miniinsta.api.repository;
-
-import com.miniinsta.api.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-    // Пока пустой, стандартных методов (save, findAll) нам хватит
-}
